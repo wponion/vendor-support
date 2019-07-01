0e2710f553cf212442d7f1781059ccc5d1720f29
Varun <varunsridharan23@gmail.com>
2019-07-01 09:20:02 +0530

    Bump flatpickr from 4.5.7 to 4.6.1

--- src/js/flatpickr.js
+++ src/js/flatpickr.js
@@ -6,6 +6,6 @@
 //@wponion-append ../../node_modules/flatpickr/dist/plugins/rangePlugin.js
 //@wponion-append ../../node_modules/flatpickr/dist/plugins/scrollPlugin.js
 //@wponion-append ../../node_modules/flatpickr/dist/plugins/confirmDate/confirmDate.js
-//@wponion-append ../../node_modules/flatpickr/dist/plugins/monthSelect/monthSelect.js
+//@wponion-append ../../node_modules/flatpickr/dist/plugins/monthSelect/index.js
 //@wponion-append ../../node_modules/flatpickr/dist/plugins/labelPlugin/labelPlugin.js
 //@wponion-append ../../node_modules/flatpickr/dist/plugins/weekSelect/weekSelect.js
